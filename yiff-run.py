e0cafb526ce5bceeff3eb6794e0ce86c367e011b
Pavel Zhadobin <pasharet@yandex.ru>
2025-04-11 23:21:44 +0300

    UwU

--- yiff-run.py
+++ yiff-run.py
@@ -1,78 +1,99 @@
 import ctypes
+import os
+import random
 import subprocess
 
-ctypes.windll.kernel32.SetConsoleTitleW("yiff-run")
+ctypes.windll.kernel32.SetConsoleTitleW("yiff-run-01042025")
+
+gguf_nvme = r'C:\AI\GGUF\\'
+gguf_hdd = r'D:\BACKUP\AI\GGUF\\'
+
+ascii_title = """
+┬ ┬┬┌─┐┌─┐  ┬─┐┬ ┬┌┐┌
+└┬┘│├┤ ├┤───├┬┘│ ││││
+ ┴ ┴└  └    ┴└─└─┘┘└┘
+"""
+
+title = [ascii_title]
 
 models = {
     # RP/ERP (Main)
-    'Yi-1.5 FT: magnum-v3-34b-Q5_K_M': {
-        'path': r'D:\BACKUP\AI\GGUF\magnum-v3-34b-Q5_K_M.gguf',
+    ' magnum-v3-34b-Q5_K_M (Yi-1.5)': {
+        'path': gguf_hdd + 'magnum-v3-34b-Q5_K_M.gguf',
         'contextsize': 8192,
         'gpulayers': 10
     },
-    'Qwen 2.5 FT: magnum-v4-72b-Q4_K_M': {
-        'path': r'D:\BACKUP\AI\GGUF\magnum-v4-72b-Q4_K_M.gguf',
+    ' magnum-v4-72b-Q4_K_M (Qwen2.5)': {
+        'path': gguf_hdd + 'magnum-v4-72b-Q4_K_M.gguf',
         'contextsize': 16384,
         'gpulayers': 0
     },
-    'Miqu 1 FT: Midnight-Miqu-70B-v1.5.Q4_K_M': {
-        'path': r'D:\BACKUP\AI\GGUF\Midnight-Miqu-70B-v1.5.Q4_K_M.gguf',
-        'contextsize': 8192,
+    ' Midnight-Miqu-70B-v1.5.Q4_K_M (Miqu 1)': {
+        'path': gguf_hdd + 'Midnight-Miqu-70B-v1.5.Q4_K_M.gguf',
+        'contextsize': 8192, # Miqu 1 - 8K
         'gpulayers': 0
     },
-    # RP/ERP (Lifeless)
-    'Gemma 2 FT: Cydonia-22B-v2k-Q6_K': {
-        'path': r'D:\BACKUP\AI\GGUF\Cydonia-22B-v2k-Q6_K.gguf',
+    ' Cydonia-22B-v2k-Q6_K (Mistral Small 2409)': {
+        'path': gguf_hdd + 'Cydonia-22B-v2k-Q6_K.gguf',
         'contextsize': 16384,
-        'gpulayers': 15
-    },
-    'Gemma 2 FT: magnum-v4-22b-Q8_0': {
-        'path': r'D:\BACKUP\AI\GGUF\magnum-v4-22b-Q8_0.gguf',
-        'contextsize': 16384,
-        'gpulayers': 15
-    },
-    'Mistral Small FT: magnum-v4-27b-Q5_K_M': {
-        'path': r'D:\BACKUP\AI\GGUF\magnum-v4-27b-Q5_K_M.gguf',
-        'contextsize': 8192,
-        'gpulayers': 10
-    },
-    'Mistral Nemo FT: saiga_nemo_12b.Q8_0': {
-        'path': r'D:\BACKUP\AI\GGUF\saiga_nemo_12b.Q8_0.gguf',
-        'contextsize': 16384,
-        'gpulayers': 20
+        'gpulayers': 14
     },
     # CODING & ASSISTANT
-    'Base: qwen2.5-coder-32b-instruct-q5_k_m': {
-        'path': r'C:\AI\GGUF\qwen2.5-coder-32b-instruct-q5_k_m.gguf',
+    ' Meta-Llama-3.1-8B-Instruct-Q5_K_M': {
+        'path': gguf_hdd + 'Meta-Llama-3.1-8B-Instruct-Q5_K_M.gguf',
+        'contextsize': 4096,
+        'gpulayers': 33 # ALL layers!
+    },
+    ' gemma-3-27b-it-abliterated.q5_k_m': {
+        'path': gguf_hdd + 'gemma-3-27b-it-abliterated.q5_k_m.gguf',
+        'contextsize': 16384,
+        'gpulayers': 12
+    },
+    ' qwen2.5-coder-32b-instruct-q5_K_M': {
+        'path': gguf_hdd + 'qwen2.5-coder-32b-instruct-q5_k_m.gguf',
         'contextsize': 8192,
         'gpulayers': 10
     },
-    'Base: c4ai-command-r-08-2024-Q5_K_M (32.3B)': {
-        'path': r'D:\BACKUP\AI\GGUF\c4ai-command-r-08-2024-Q5_K_M.gguf',
+    ' qwq-32b-q5_k_m': {
+        'path': gguf_hdd + 'qwq-32b-q5_k_m.gguf',
         'contextsize': 8192,
         'gpulayers': 10
     },
-    'Qwen 2.5 FT: t-pro-it-1.0-q5_k_m (32.8B) - NOT ready-to-use': {
-        'path': r'D:\BACKUP\AI\GGUF\t-pro-it-1.0-q5_k_m.gguf',
+    ' DeepSeek-R1-Distill-Qwen-32B-Q5_K_M (Qwen2.5)': {
+        'path': gguf_hdd + 'DeepSeek-R1-Distill-Qwen-32B-Q5_K_M.gguf',
         'contextsize': 8192,
         'gpulayers': 10
-    }
+    },
+    'c4ai-command-r-08-2024-Q5_K_M': {
+        'path': gguf_hdd + 'c4ai-command-r-08-2024-Q5_K_M.gguf',
+        'contextsize': 8192,
+        'gpulayers': 10
+    },
 }
 
 def run_model(model_name, model_info):
     command = [
-        r'C:\AI\koboldcpp_cu12.exe',
+        r'C:\_ai\koboldcpp_cu12.exe',
         '--model', model_info['path'],
         '--threads', '6',
         '--usecublas', 'normal', 'mmq', '0',
+        # '--usecublas', 'normal', '0',
         '--contextsize', str(model_info['contextsize']),
         '--gpulayers', str(model_info['gpulayers']),
+        '--skiplauncher',
         '--blasbatchsize', '512',
         '--blasthreads', '6',
-        '--skiplauncher',
+        # '--usemmap',
+        '--nommap',
+        # '--usemlock',
         '--multiuser', '2',
+        '--multiplayer',
+        '--highpriority',
         '--quiet',
-        '--password', 'yiff'
+        '--password', 'knotting',
+        # '--flashattention',
+        # '--benchmark'
+        # '--moeexperts', '8',
     ]
     
     try:
@@ -81,13 +102,14 @@ def run_model(model_name, model_info):
         print(f"Ошибка при запуске модели {model_name}: {e}")
 
 def main():
-    print("yiff-run-080125\n")
+    os.system('cls')
+    print(random.choice(title))
     for i, (model_name, model_info) in enumerate(models.items(), start=1):
         print(f"{i}. {model_name}")
 
     while True:
         try:
-            choice = int(input("Введите номер модели: "))
+            choice = int(input("\nВведите номер модели: "))
             if 1 <= choice <= len(models):
                 selected_model_name = list(models.keys())[choice - 1]
                 break
@@ -96,9 +118,8 @@ def main():
         except ValueError:
             print("Пожалуйста, введите число.")
 
-    print("")
+    os.system('cls')
     subprocess.run(['taskkill', '/f', '/im', 'koboldcpp_cu12.exe'], check=False)
-    print("")
 
     model_info = models[selected_model_name]
     run_model(selected_model_name, model_info)
